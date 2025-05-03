5c5db62abb5b35143f2344eda2b1622acb624693
Amr Badawy <amr.badawy@allup.app>
2025-05-03 13:40:13 +0300

    fix vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
+import url from 'node:url'
+
+const _dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(_dirname, "./src"),
+    },
   },
   server: {
     port: 8888,
